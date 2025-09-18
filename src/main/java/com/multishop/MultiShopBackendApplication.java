f48057c5ac6f2efd90e1102271c592428282c5b2
robinjuyal <85496825+robinjuyal@users.noreply.github.com>
2025-09-18 23:18:58 +0530

    initial commit

---
+++ src/main/java/com/multishop/MultiShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.multishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiShopBackendApplication.class, args);
+	}
+
+}
